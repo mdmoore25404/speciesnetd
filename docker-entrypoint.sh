2e43c73ae080b8bed1e195d94fab588ae104a1bd
mdmoore25404 <github@mdmoore.me>
2025-03-28 00:34:03 +0000

    Add GPU test execution after speciesnet installation in entrypoint script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,6 +10,10 @@ if ! pip install  "speciesnet>=4.0.3,<4.1.0"; then
     exit 1
 fi
 
+python -m speciesnet.scripts.gpu_test
+
+
+
 
 # Check if USE_UWSGI environment variable is set and not false
 if [ "${USE_UWSGI:-false}" != "false" ]; then
